9e8f02692802e07bac1024f4dddada00634f1ddb
Joseph Camyre <jwcamry03@gmail.com>
2021-10-06 21:50:46 -0700

    .

--- backend/settings.py
+++ backend/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # points to directory/folder that stores static files
