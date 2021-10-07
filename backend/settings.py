8b357d54c92c0942819166633104cec89cbf1ed7
Joseph Camyre <jwcamry03@gmail.com>
2021-10-07 13:34:38 -0700

    .

--- backend/settings.py
+++ backend/settings.py
@@ -149,12 +149,12 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'build/static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
 
 # # points to directory/folder that stores static files
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
